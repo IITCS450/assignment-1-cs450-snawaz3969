b1f93649e286d4fd76dcce441d096171a044380c
snawaz3969 <snawaz1@hawk.illinoistech.edu>
2026-02-01 21:58:32 -0600

    Update runwait.c

--- src/runwait.c
+++ src/runwait.c
@@ -1,3 +1,4 @@
+#define _POSIX_C_SOURCE 200809L
 #include "common.h"
 #include <sys/wait.h>
 #include <time.h>
@@ -6,7 +7,37 @@ static void usage(const char *a){fprintf(stderr,"Usage: %s <cmd> [args]\n",a); e
 static double d(struct timespec a, struct timespec b){
  return (b.tv_sec-a.tv_sec)+(b.tv_nsec-a.tv_nsec)/1e9;}
 int main(int c,char**v){
-/* TODO : ADD CODE HERE
-*/
+// TODO : ADD CODE HERE
+
+    if (c < 2) usage(v[0]);
+
+    struct timespec start, end;
+    if (clock_gettime(CLOCK_MONOTONIC, &start) < 0)
+        DIE("clock_gettime");
+
+    pid_t pid = fork();
+    if (pid < 0) {
+        DIE("fork");
+    } else if (pid == 0) {
+        execvp(v[1], &v[1]);
+        DIE("execvp");
+    }
+
+    int status;
+    if (waitpid(pid, &status, 0) < 0)
+        DIE("waitpid");
+
+    if (clock_gettime(CLOCK_MONOTONIC, &end) < 0)
+        DIE("clock_gettime");
+
+    printf("pid=%d", pid);
+    printf(" elapsed=%.3f seconds", d(start, end));
+
+    if (WIFEXITED(status)) {
+        printf(" exit=%d\n", WEXITSTATUS(status));
+    } else if (WIFSIGNALED(status)) {
+        printf(" signal=%d\n", WTERMSIG(status));
+    }
+
  return 0;
 }
